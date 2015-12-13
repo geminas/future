a869eb773e239c30acad2ef8008d78b3ee121f80
huangyan9188 <huangyan9188@163.com>
2015-12-13 18:22:03 +0800

    add the changes

---
+++ htdocs/contactus.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta id="viewport" name="viewport" content="width=device-width">
+    
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap_1200.css" rel="stylesheet">
+    <!-- custom css-->
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="screen">
+    <link href="css/donghai.css" rel="stylesheet" type="text/css">
+
+
+    
+</head>
+
+<body style="background-color:#939598; ">
+<?php require "headermenu.php" ?>
+
+<div class="container" style="padding:30px">
+    <div class="row tabs">
+        <div class="col-sm-8 text-center col-sm-offset-2">
+           <ul class="nav nav-tabs ">
+              <li ><a href="/aboutforum.php">关于未来论坛</a></li>
+              <li><a href="/aboutus.php">关于我们</a></li>
+              <li class=""><a href="/product.php">产品服务</a></li>
+              <li><a href="/joinus.php">加入我们</a></li>
+              <li class="active"><a href="#">联系我们</a></li>
+            </ul>
+        </div>
+        <div class="col-sm-8 col-sm-offset-2 about-content">
+                <h1 class="t-h1">联系我们</h1>
+                <div class="ctt-wrap">
+                    <p><br><span style="line-height: 1.8em;"><strong>基本数据</strong></span><br>每月1500万浏览量<br>用户平均浏览页面4.75，平均访问时长18分钟<br>总计50+万 新浪微博、腾讯微博粉丝<br>总计200+万 搜狐新闻客户端、网易云阅读、鲜果等阅读平台订阅用户<br>Alexa：全球排名 1200—3000，国内排名：100——300</p><p><br></p><p><br><span style="line-height: 1.8em;"><strong>虎嗅用户</strong></span><br>男性读者占比79.26%<br>25-49岁用户占比70%<br>75%用户分布北上广、浙江、海外<br>本科以上学历用户占比70%<br>移动设备访问占比30%</p><p><br></p><p><br><span style="line-height: 1.8em;"><strong>虎嗅内容渠道</strong></span><br>虎嗅微信公众平台<br>虎嗅APP<br>虎嗅研究报告<br>电子书</p><p><br></p><p><br><span style="line-height: 1.8em;"><strong>虎嗅产品及服务</strong></span><br>整合传播&nbsp;<span style="line-height: 1.8em;">&nbsp;</span></p><p>客户定制会议<br>虎嗅培训</p><p><br></p><p><br><span style="line-height: 1.8em;"><strong>虎嗅广告</strong></span><br>站内广告<br>定制化广告<br>微信广告<br>APP广告</p><p><br></p><p><br><span style="line-height: 1.8em;"><strong>如果您希望与虎嗅开展广告及其它商务合作，请发邮件至<a href="mailto:marketing@huxiu.com" target="_blank">marketing@huxiu.com</a></strong></span><br>邮件中请注明公司名称，联系方式，具体合作需求，我们将在24小时之内尽快与您联系。</p><p>媒体合作请联系&nbsp;<span style="line-height: 1.8em;">hezuo@huxiu.com (内容转载、联合活动)&nbsp;</span></p><p><br></p>                </div>
+        </div>
+    </div>
+</div>
+
+
+
+<?php
+    require "footer.php";
+?>
+
+    <!--scripts and plugins -->
+    <!--must need plugin jquery-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/donghai.js"></script>
+    <!--bootstrap js plugin-->
+    <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    
+    <script>
+        //$(document).ready(adaptiveContainer);
+        //$(window).resize(adaptiveContainer);
+        
+
+    </script>
+    
+</body>
+
+</html>
---
+++ htdocs/footer.php
@@ -0,0 +1,81 @@
+<div class="section">
+    <div class="hpsection container GDH-normalpadding">
+        <div class="row">
+            <div class="col-md-5" style="margin-bottom:40px">
+                <img src="contents/logo-grey.png" style="width: 170px;">
+            </div>
+            <!--footer col-->
+            <div class="col-md-7">
+                <div class="row">
+                    <div class="col-xs-6 col-sm-4" >
+                        <div style="margin-bottom:40px">
+                            <div style="background-color:black; width:100%; height:6px"></div>
+                            <h3 class="FooterHeader">战略支持<br>Sponsor</h3>
+                            <a href="http://www.infiniti.com.cn"><img style="width:100%; padding-bottom:5px" src="contents/sponsors/infiniti.png"></a>
+                        </div>
+                        <div style="margin-bottom:40px">
+                            <div style="background-color:black; width:100%; height:6px"></div>
+                            <h3 class="FooterHeader">执行机构<br>Executive Agency</h3>
+                            <a href="http://www.alliance.com.cn/"><img style="width:100%; padding-bottom:5px" src="contents/sponsors/xinmeng.png"></a>
+                        </div>
+                    </div>
+                    <div class="col-xs-6 col-sm-4">
+                        <div style="margin-bottom:40px">
+                            <div>
+                                <div style="background-color:black; width:100%; height:6px"></div>
+                                <h3 class="FooterHeader">战略合作媒体<br>Media Coalition</h3>
+                                <a href="http://news.baidu.com"><img style="width:100%; padding-bottom:5px" src="contents/sponsors/baidunews.png"></a>
+                                <a href="http://tech.sina.com.cn"><img style="width:100%; padding-bottom:5px" src="contents/sponsors/xinlangkeji.png"></a>
+                                <a href="http://www.huanqiukexue.com"><img style="width:100%; padding-bottom:5px" src="contents/sponsors/huanqiukexue.png"></a>
+                                <a href="http://www.iqiyi.com"><img style="width:100%; padding-bottom:5px" src="contents/sponsors/iqiyi.png"></a>
+                                <a href="http://open.163.com"><img style="width:100%; padding-bottom:5px" src="contents/sponsors/wangyigongkaike.png"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-4">
+                        <div class="row">
+                            <div class="col-xs-6 col-sm-12" style="margin-bottom:40px">
+                                <div style="background-color:black; width:100%; height:6px"></div>
+
+                                <h3 class="FooterHeader">联系方式<br>Contact Information</h3>
+                                <table style="width:100%;" class="FooterContent">
+                                    <tr>
+                                        <td><i class="fa fa-map"></i>
+                                        </td>
+                                        <td>北京市朝阳区朝外大街甲6号
+                                            <br>万通中心D座803, 100020</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i class="fa fa-phone"></i>
+                                        </td>
+                                        <td>+86 10 58751688</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i class="fa fa-print"></i>
+                                        </td>
+                                        <td>+86 10 58751601</td>
+                                    </tr>
+                                    <tr>
+                                        <td><i class="fa fa-envelope"></i>
+                                        </td>
+                                        <td>futureforum@alliance.com.cn</td>
+                                    </tr>
+                                </table>
+                            </div>
+                            <div class="col-xs-6 col-sm-12" style="margin-bottom:40px">
+                                <div style="background-color:black; width:100%; height:6px"></div>
+                                <h3 class="FooterHeader">官方微信订阅号<br>WeChat Subscription</h3>
+                                <div style="text-align: right">
+                                    <img style="width:100%" src="contents/wechat.png">
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+            <!--footer col-->
+        </div>
+    </div>
+</div>
