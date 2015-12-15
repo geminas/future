09fd759c6a6c1f712d7097ea6a102f9a04d2445a
jiangqichen <jiangqichen@xiaoma.cn>
2015-12-15 21:55:43 +0800

    关于我们全部粘贴

--- htdocs/contactus.php
+++ htdocs/contactus.php
@@ -33,7 +33,23 @@
         <div class="col-sm-8 col-sm-offset-2 about-content">
                 <h1 class="t-h1">联系我们</h1>
                 <div class="ctt-wrap">
-                    <p><br><span style="line-height: 1.8em;"><strong>基本数据</strong></span><br>每月1500万浏览量<br>用户平均浏览页面4.75，平均访问时长18分钟<br>总计50+万 新浪微博、腾讯微博粉丝<br>总计200+万 搜狐新闻客户端、网易云阅读、鲜果等阅读平台订阅用户<br>Alexa：全球排名 1200—3000，国内排名：100——300</p><p><br></p><p><br><span style="line-height: 1.8em;"><strong>虎嗅用户</strong></span><br>男性读者占比79.26%<br>25-49岁用户占比70%<br>75%用户分布北上广、浙江、海外<br>本科以上学历用户占比70%<br>移动设备访问占比30%</p><p><br></p><p><br><span style="line-height: 1.8em;"><strong>虎嗅内容渠道</strong></span><br>虎嗅微信公众平台<br>虎嗅APP<br>虎嗅研究报告<br>电子书</p><p><br></p><p><br><span style="line-height: 1.8em;"><strong>虎嗅产品及服务</strong></span><br>整合传播&nbsp;<span style="line-height: 1.8em;">&nbsp;</span></p><p>客户定制会议<br>虎嗅培训</p><p><br></p><p><br><span style="line-height: 1.8em;"><strong>虎嗅广告</strong></span><br>站内广告<br>定制化广告<br>微信广告<br>APP广告</p><p><br></p><p><br><span style="line-height: 1.8em;"><strong>如果您希望与虎嗅开展广告及其它商务合作，请发邮件至<a href="mailto:marketing@huxiu.com" target="_blank">marketing@huxiu.com</a></strong></span><br>邮件中请注明公司名称，联系方式，具体合作需求，我们将在24小时之内尽快与您联系。</p><p>媒体合作请联系&nbsp;<span style="line-height: 1.8em;">hezuo@huxiu.com (内容转载、联合活动)&nbsp;</span></p><p><br></p>                </div>
+                    <div>
+                        <p align="left">
+                            【联系我们】：</p>
+                        <p>
+                            地址：北京市朝阳区朝外大街甲6号万通中心D座803, 100020</p>
+                        <p>
+                            电话：+86 10 58751688</p>
+                        <p align="left">
+                            Email：<a href="mailto:ff2016@futureforum.com.cn">ff2016@futureforum.com.cn</a></p>
+                        <p align="left">
+                            &nbsp;</p>
+                        <p align="left">
+                            微博：@<a href="http://weibo.com/futureforum">未来论坛FutureForum</a></p>
+                    </div>
+                    <p>
+                        &nbsp;</p>
+                </div>
         </div>
     </div>
 </div>
--- htdocs/product.php
+++ htdocs/product.php
@@ -33,7 +33,45 @@
         <div class="col-sm-8 col-sm-offset-2 about-content">
                 <h1 class="t-h1">产品服务</h1>
                 <div class="ctt-wrap">
-                    <p><br><span style="line-height: 1.8em;"><strong>基本数据</strong></span><br>每月1500万浏览量<br>用户平均浏览页面4.75，平均访问时长18分钟<br>总计50+万 新浪微博、腾讯微博粉丝<br>总计200+万 搜狐新闻客户端、网易云阅读、鲜果等阅读平台订阅用户<br>Alexa：全球排名 1200—3000，国内排名：100——300</p><p><br></p><p><br><span style="line-height: 1.8em;"><strong>虎嗅用户</strong></span><br>男性读者占比79.26%<br>25-49岁用户占比70%<br>75%用户分布北上广、浙江、海外<br>本科以上学历用户占比70%<br>移动设备访问占比30%</p><p><br></p><p><br><span style="line-height: 1.8em;"><strong>虎嗅内容渠道</strong></span><br>虎嗅微信公众平台<br>虎嗅APP<br>虎嗅研究报告<br>电子书</p><p><br></p><p><br><span style="line-height: 1.8em;"><strong>虎嗅产品及服务</strong></span><br>整合传播&nbsp;<span style="line-height: 1.8em;">&nbsp;</span></p><p>客户定制会议<br>虎嗅培训</p><p><br></p><p><br><span style="line-height: 1.8em;"><strong>虎嗅广告</strong></span><br>站内广告<br>定制化广告<br>微信广告<br>APP广告</p><p><br></p><p><br><span style="line-height: 1.8em;"><strong>如果您希望与虎嗅开展广告及其它商务合作，请发邮件至<a href="mailto:marketing@huxiu.com" target="_blank">marketing@huxiu.com</a></strong></span><br>邮件中请注明公司名称，联系方式，具体合作需求，我们将在24小时之内尽快与您联系。</p><p>媒体合作请联系&nbsp;<span style="line-height: 1.8em;">hezuo@huxiu.com (内容转载、联合活动)&nbsp;</span></p><p><br></p>                </div>
+                    <div>
+                        <p align="left">
+                            <strong>&mdash;&mdash;</strong><strong>基本数据</strong><strong>&mdash;&mdash;</strong></p>
+                        <p>
+                            微博粉丝数：6K+（截止2015/12/25）<br />
+                            微信粉丝数：6K+（截止2015/12/25）</p>
+                        <p>
+                            新闻客户端订阅： 网易、凤凰、腾讯、今日头条、一点资讯、知道日报等阅读平台 （总计10K订阅用户）</p>
+                        <p align="left">
+                            <strong>&mdash;&mdash;</strong><strong>主要渠道</strong><strong>&mdash;&mdash;</strong></p>
+                        <p>
+                            未来论坛官网：<a href="http://www.futureforum.org.cn/">http://www.futureforum.org.cn/</a><br />
+                            未来论坛官方微博：@<a href="http://weibo.com/futureforum">未来论坛FutureForum</a><br />
+                            未来论坛微信公众平台：未来论坛（ID：futureforum）&nbsp;</p>
+                        <p>
+                            各大新闻客户端：<a href="http://c.3g.163.com/nc/qa/rss/default.html#/share/T1444889666048">网易</a>、凤凰、<a href="http://dy.qq.com/list-user.htm?chlid=5007059">腾讯</a>、<a href="http://mp.i.sohu.com/suc298684725/profile">搜狐</a>、<a href="http://www.yidianzixun.com/home?page=channel&amp;id=m35399">一点资讯</a>、<a href="http://toutiao.com/m5547045596/">今日头条</a>、<a href="http://weilailuntan.baijia.baidu.com/">百度百家</a>、<a href="http://zhidao.baidu.com/daily/author/?un=%CE%B4%C0%B4%C2%DB%CC%B3&amp;ie=gbk">知道日报</a><br />
+                            视频内容：<a href="http://open.163.com/special/openclass/weilailuntan.html">网易公开课</a>、<a href="http://www.iqiyi.com/a_19rrhb5rot.html">爱奇艺</a>、<a href="http://tech.sina.com.cn/z/futureforum10/#139059644">新浪专题</a></p>
+                        <p align="left">
+                            <strong>&mdash;&mdash;</strong><strong>线下活动</strong><strong>&mdash;&mdash;</strong></p>
+                        <p align="left">
+                            在过去的时间里，未来论坛每月举办一期科普性质的&ldquo;理解未来&rdquo;讲座；每季度举办一次&ldquo;闭门耕&rdquo;研讨会议；以及每年举办一次的盛大的&ldquo;未来年会&rdquo;。在2016年1月17日未来论坛首届年会中，创建中国首个由民间发起的诺贝尔&ldquo;未来科学前沿大奖&rdquo;。</p>
+                        <p align="left">
+                            <strong>月度讲座：</strong><strong>&ldquo;</strong><strong>理解未来</strong><strong>&rdquo;</strong></p>
+                        <p align="left">
+                            &ldquo;理解未来&rdquo;月度讲座，覆盖物理、化学、数学、生命科学、医疗、科技等众多领域，所邀请的嘉宾悉数全球顶尖科学家&mdash;&mdash;中科院高能物理研究所张双南；&ldquo;千人计划&rdquo;入选者潘建伟；哈佛大学Mallinckrodt讲席教授谢晓亮。话题更是从人工智能、未来计算机，到星际穿越，基因重组，再到大数据精准医疗，丰富而精彩。</p>
+                        <p align="left">
+                            同时，为使科学学习更加趣味化，让更多人爱上科学，除了线下&ldquo;理解未来&rdquo;月度讲座和&ldquo;闭门耕&rdquo;季度研讨外，未来论坛以互联网思维、线上线下互动机制，创造了一个全新的专注于科学的公益平台。通过线上官网、微博、微信，聚集了15000人以上的粉丝；透过网易公开课、今日头条、腾讯、新浪、凤凰、网易、搜狐等多家自媒体合作渠道，实现文章阅读、视频播放约1000万人次的浏览、观看。</p>
+                        <p align="left">
+                            <strong>季度研讨：</strong><strong>&ldquo;</strong><strong>闭门耕</strong><strong>&rdquo;</strong></p>
+                        <p>
+                            &ldquo;闭门耕&rdquo;研讨沙龙每季半天，是小型而私密的头脑风暴。对最热点、最前沿技术、投资领域，具有针对性的&ldquo;耕耘&rdquo;研讨话题。以前瞻性、启发性和思辨性为主，包括技术、商业模式和资源整合等方面。参加群体主要针对理事会成员，理事机构、赞助机构、定向嘉宾邀请，进行话题的深度探求、互动交流，碰撞激发，打通产、学、研以及资本对接的深度交流，创造行业标准,引领行业未来的发展方向。</p>
+                        <p align="left">
+                            <strong>年度盛宴：</strong><strong>&ldquo;</strong><strong>未来年会</strong><strong>&rdquo;</strong></p>
+                        <p>
+                            未来年会，在创始理事、咨委会员、科学界权威、政商界领袖及艺术界精英等人群的参与下，自创立大会之初，便成为了一年一度的全球科学人文创新的盛宴。年度综合性大型高端论坛，聚焦对未来有所洞见的人，秉承生生不息的科学思想、人文精神，启迪创新思维，引领未来。&nbsp;</p>
+                    </div>
+                    <p>
+                        &nbsp;</p>
+                </div>
         </div>
     </div>
 </div>
