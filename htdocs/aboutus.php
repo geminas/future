c35c7b221e0a3e3d67472171d499ee98c5218203
jiangqichen <jiangqichen@xiaoma.cn>
2015-12-19 14:28:33 +0800

    链接 居左

--- htdocs/aboutus.php
+++ htdocs/aboutus.php
@@ -36,21 +36,14 @@
                 <h4 class="t-h1">关于我们</h4>
                 <div class="ctt-wrap">
                     <div style="font-size: 16px">
+                        <p><strong>&mdash;&mdash;</strong><strong>未来论坛</strong><strong>&middot;</strong><strong>是谁</strong><strong>&mdash;&mdash;</strong></p>
+                        <p>未来论坛，是一个民办非企业性质的公益组织，是承载人类科技梦想，用科技改变世界的公益平台；也是一个连接前沿科技，解读未来趋势的思想平台。</p>
+                        <p><strong>&mdash;&mdash;</strong><strong>未来论坛</strong><strong>&middot;</strong><strong>创建</strong><strong>&amp;</strong><strong>宗旨</strong><strong>&mdash;&mdash;</strong></p>
+                        <p>未来论坛，最初由一批极具影响力的互联网界、投资界、科学界、教育界领袖共同发起。旨在用科学系统化的方法，在传播科学思想、人文精神的同时，推动启蒙的浪潮，用科学的维度，理性的维度探索未来。</p>
+                        <p><strong>&mdash;&mdash;</strong><strong>未来论坛</strong><strong>&middot;</strong><strong>构架</strong><strong>&mdash;&mdash;</strong></p>
+                        <p><strong>未来论坛</strong><strong> | </strong><strong>理事会</strong></p>
+                        <p>未来论坛理事会由投资、实业、教育等跨领域行业领袖组成，他们具有良好的社会声誉与极强的社会影响力。他们是一群有理想、有思想且志同道合的精英人群。</p>
                         <p align="left">
-                            <strong>&mdash;&mdash;</strong><strong>未来论坛</strong><strong>&middot;</strong><strong>是谁</strong><strong>&mdash;&mdash;</strong></p>
-                        <p align="left">
-                            未来论坛，是一个民非企业性质的公益组织，是承载人类科技梦想，用科技改变世界的公益平台；也是一个连接前沿科技，解读未来趋势的思想平台。</p>
-                        <p align="left">
-                            <strong>&mdash;&mdash;</strong><strong>未来论坛</strong><strong>&middot;</strong><strong>创建</strong><strong>&amp;</strong><strong>宗旨</strong><strong>&mdash;&mdash;</strong></p>
-                        <p align="left">
-                            未来论坛，最初由一批极具影响力的互联网界、投资界、科学界、教育界领袖共同发起。旨在用科学系统化的方法，在传播科学思想、人文精神的同时，推动启蒙的浪潮，用科学的维度，理性的维度探索未来。</p>
-                        <p align="left">
-                            <strong>&mdash;&mdash;</strong><strong>未来论坛</strong><strong>&middot;</strong><strong>构架</strong><strong>&mdash;&mdash;</strong></p>
-                        <p align="left">
-                            <strong>未来论坛</strong><strong> | </strong><strong>理事会</strong></p>
-                        <p>
-                            未来论坛理事会由经济、科学、教育等跨领域行业领袖组成，他们具有良好的社会声誉与极强的社会影响力。他们是一群有理想、有思想且志同道合的精英人群。</p>
-                        <p>
                             陈恂，Basebit Technologies创始人、首席执行官</p>
                         <p align="left">
                             邓锋，北极光创始人兼董事总经理</p>
@@ -60,7 +53,7 @@
                             方方，水木投资集团创始人兼董事长</p>
                         <p align="left">
                             冯仑，万通集团主席，万通投资控股股份有限公司董事长</p>
-                        <p>
+                        <p align="left">
                             李柏霖，通邮集团董事长</p>
                         <p align="left">
                             李彦宏，百度公司创始人、董事长兼首席执行官</p>
@@ -87,42 +80,65 @@
                         <p align="left">
                             张醒生，道同资本创始合伙人，大自然保护协会亚太区首席代表，老牛基金会理事长</p>
                         <p align="left">
-                            <strong>未来论坛</strong><strong> | </strong><strong>咨询委员会</strong></p>
-                        <p>
-                            未来论坛，网罗一批既具有国内学术、金融、产业类事业单位多年执业经历，又各自掌握领域尖端技术与雄厚资源，且关注国家科学发展的有识之士组成未来论坛咨询委员会。</p>
-                        <p>高西庆，清华大学法学院郑裕彤讲席教授，中国投资有限责任公司原副董事长、总经理兼首席执行官</p>
-                        <p>康典，新华人寿保险有限公司董事长、执行董事</p>
-                        <p>李剑阁，孙冶方经济科学基金会理事长</p>
-                        <p>廖晓淇，国贸董事长、原中国商务部副部长</p>
-                        <p>饶毅，北京大学终身讲席教授、北京大学校务委员会副主任，兼北京生命科学研究所资深研究员</p>
-                        <p>施一公，清华大学副校长、清华大学生命科学学院院长、中国科学院院士</p>
-                        <p>衣锡群，中国股权投资基金协会、常务副会长</p>
-                        <p>张首晟，斯坦福大学物理系讲座教授，清华大学高等研究院千人计划访问教授，美国艺术与科学学院院士</p>
-                        <p>朱云来，著名金融人士</p>
+                            <strong>未来论坛</strong><strong>&nbsp;|&nbsp;</strong><strong>咨询委员会</strong></p>
                         <p align="left">
-                            <strong>未来论坛</strong><strong> | </strong><strong>科学家顾问委员会</strong></p>
-                        <p>
-                            未来论坛，携手全球顶尖科学家、科技领袖，汇聚国内外在顶级大学任职，并在物理、化学、数学、生命科学等各自研究领域有所科研成就的专家、教授组成未来论坛高水准的科学家委员会。</p>
-                        <p>丁洪，中国科学院物理所研究员</p>
-                        <p>何川，芝加哥大学讲座教授、北京大学长江客座教授</p>
-                        <p>文小刚，理论物理学家、美国麻省理工学院终身教授</p>
-                        <p>饶毅，北京大学终身讲席教授、北京大学校务委员会副主任，兼北京生命科学研究所资深研究员</p>
-                        <p>施一公，清华大学副校长、清华大学生命科学学院院长、中国科学院院士</p>
-                        <p>田刚，北京大学教授、中国科学院院士</p>
-                        <p>王晓东，北京生命科学研究所所长，百济神州创始人，美国科学院院士</p>
-                        <p>谢晓亮，哈佛大学Mallinckrodt 讲席教授，北京大学生物动态光学成像中心主任，美国国家科学院院士</p>
-                        <p>夏志宏，数学家，美国西北大学终身教授北京大学数学科学学院长江学者讲座教授与博士生导师</p>
+                            未来论坛，网罗一批既具有国内外学术、金融、产业多年执业经历，又各自掌握领域尖端技术与雄厚资源，且关注国家科学发展的有识之士组成未来论坛咨询委员会。</p>
                         <p align="left">
-                            <strong>未来论坛 </strong><strong>| </strong><strong>青年行动小组</strong></p>
-                        <p>
+                            高西庆，清华大学法学院郑裕彤讲席教授，中国投资有限责任公司原副董事长、总经理兼首席执行官</p>
+                        <p align="left">
+                            康典，新华人寿保险有限公司董事长、执行董事</p>
+                        <p align="left">
+                            李剑阁，孙冶方经济科学基金会理事长</p>
+                        <p align="left">
+                            廖晓淇，国贸董事长、原中国商务部副部长</p>
+                        <p align="left">
+                            饶毅，北京大学终身讲席教授、北京大学校务委员会副主任，兼北京生命科学研究所资深研究员</p>
+                        <p align="left">
+                            施一公，清华大学副校长、清华大学生命科学学院院长、中国科学院院士</p>
+                        <p align="left">
+                            衣锡群，中国股权投资基金协会、常务副会长</p>
+                        <p align="left">
+                            张首晟，斯坦福大学物理系讲座教授，清华大学高等研究院千人计划访问教授，美国艺术与科学学院院士</p>
+                        <p align="left">
+                            朱云来，著名金融人士</p>
+                        <p align="left">
+                            <strong>未来论坛</strong><strong>&nbsp;|&nbsp;</strong><strong>科学家顾问委员会</strong></p>
+                        <p align="left">
+                            未来论坛，携手全球顶尖科学家、科技领袖，汇聚国内外在顶级大学任职，并在数学、物理、化学、生命科学等各自研究领域有所科研成就的专家、教授组成未来论坛高水准的科学家委员会。</p>
+                        <p align="left">
+                            丁洪，中国科学院物理所研究员</p>
+                        <p align="left">
+                            何川，芝加哥大学讲座教授、北京大学长江客座教授</p>
+                        <p align="left">
+                            饶毅，北京大学终身讲席教授、北京大学校务委员会副主任，兼北京生命科学研究所资深研究员</p>
+                        <p align="left">
+                            施一公，清华大学副校长、清华大学生命科学学院院长、中国科学院院士</p>
+                        <p align="left">
+                            田刚，北京大学教授、中国科学院院士</p>
+                        <p align="left">
+                            王晓东，北京生命科学研究所所长，百济神州创始人，美国科学院院士</p>
+                        <p align="left">
+                            文小刚，理论物理学家、美国麻省理工学院终身教授</p>
+                        <p align="left">
+                            夏志宏，数学家，美国西北大学终身教授北京大学数学科学学院长江学者讲座教授与博士生导师</p>
+                        <p align="left">
+                            谢晓亮，哈佛大学Mallinckrodt 讲席教授，北京大学生物动态光学成像中心主任，美国国家科学院院士</p>
+                        <p align="left">
+                            <strong>未来论坛</strong><strong>&nbsp;|&nbsp;</strong><strong>青年行动小组</strong></p>
+                        <p align="left">
                             未来论坛，聚集了一群40岁以下，拥有创新思维与专业技术，在中国最具影响力的互联网青年创业者组成青年行动小组，透过未来论坛，鼓励并支持新生代青年的创业梦想及理念，让梦想落地。</p>
-                        <p>
+                        <p align="left">
+                            戴雷，东风英菲尼迪总经理</p>
+                        <p align="left">
                             山世光，中科院计算所研究员</p>
-                        <p>赵勇，格灵深瞳联合创始人</p>
-                        <p>王俊，碳云智能创始人、CEO</p>
-                        <p>余凯，Horizon Robotics创始人</p>
-                        <p>王淮，线性资本创始合伙人</p>
-                        <p>戴雷，东风英菲尼迪总经理</p>
+                        <p align="left">
+                            王淮，线性资本创始合伙人</p>
+                        <p align="left">
+                            王俊，碳云智能创始人、CEO</p>
+                        <p align="left">
+                            余凯，Horizon Robotics创始人</p>
+                        <p align="left">
+                            赵勇，格灵深瞳联合创始人</p>
                     </div>
                 </div>
         </div>
