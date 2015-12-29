bbe45747a430fd7c8f7da144538910a454227180
huangyan9188 <huangyan9188@163.com>
2015-12-29 22:34:14 +0800

    add changes

--- htdocs/newsitem.php
+++ htdocs/newsitem.php
@@ -93,7 +93,10 @@ $folder="contents/news/";
         </div>
     </div>
 </div>
-
+<!-- UY BEGIN -->
+<div id="uyan_frame"></div>
+<script type="text/javascript" src="http://v2.uyan.cc/code/uyan.js?uid=2078754"></script>
+<!-- UY END -->
 
 
 <?php
