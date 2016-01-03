8f5ee29a7fc3ab3d3094f37b4c75f5cbc674b209
jiangqichen <jiangqichen@xiaoma.cn>
2016-01-03 14:54:48 +0800

    engliah

--- htdocs/headermenu.php
+++ htdocs/headermenu.php
@@ -13,7 +13,7 @@
         <a href="index.php#action"><span style="width:11.7%" class="HPtopmenu2">未来·行动</span></a>
         <a href="footprints.php"><span style="width:11.7%" class="HPtopmenu3">未来·足迹</span></a>
         <a href="news.php"><span style="width:11.7%" class="HPtopmenu4">未来·资讯</span></a>
-        <!-- <a href="index_en.php"><span style="width:11.7%" class="HPtopmenu5">English</span></a> -->
+        <a href="index_en.php"><span style="width:11.7%" class="HPtopmenu5">English</span></a>
     </div>
     <div class="row">
         <a href="index.php">
@@ -27,7 +27,7 @@
             <a href="index.php#action"><div class="HPtopmenucollapse" style="padding: 10%;">未来·行动</div></a>
             <a href="footprints.php"><div class="HPtopmenucollapse" style="padding: 10%;">未来·足迹</div></a>
             <a href="news.php"><div class="HPtopmenucollapse" style="padding: 10%;">未来·资讯</div></a>
-            <!-- <a href="index_en.php"><div class="HPtopmenucollapse" style="padding: 10%;">English</div></a> -->
+            <a href="index_en.php"><div class="HPtopmenucollapse" style="padding: 10%;">English</div></a>
         </div>
     </div>
 </div>
