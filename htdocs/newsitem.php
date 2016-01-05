dcd168bcbd120355ca57924b0616b7a12435cae3
jiangqichen <jiangqichen@xiaoma.cn>
2016-01-06 00:07:26 +0800

    ceshi

--- htdocs/newsitem.php
+++ htdocs/newsitem.php
@@ -94,7 +94,7 @@ $folder="contents/news/";
     </div>
 </div>
 <!-- UY BEGIN -->
-<div id="uyan_frame" style="padding:30px"></div>
+<div id="uyan_frame" class="col-sm-8" style="padding:30px"></div>
 <script type="text/javascript" src="http://v2.uyan.cc/code/uyan.js?uid=2078754"></script>
 <!-- UY END -->
 
