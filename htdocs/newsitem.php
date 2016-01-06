47fb64e00bf22de0f4d9b5679915a98003292732
jiangqichen <jiangqichen@xiaoma.cn>
2016-01-06 20:08:41 +0800

    width

--- htdocs/newsitem.php
+++ htdocs/newsitem.php
@@ -95,9 +95,9 @@ $folder="contents/news/";
 </div>
 <!-- UY BEGIN -->
 
-<div class="container" style="padding:50px">
+<div class="col-sm-8" style="padding:50px">
     <div class="row">
-        <div id="uyan_frame" style="width:80%"></div>
+        <div id="uyan_frame" style="width:100%"></div>
         <script type="text/javascript" src="http://v2.uyan.cc/code/uyan.js?uid=2078754"></script>
     </div>
 </div>
