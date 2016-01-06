8a64b1caf03c584b677e7e6cc5d2b0e488c887d6
jiangqichen <jiangqichen@xiaoma.cn>
2016-01-06 23:17:22 +0800

    duole yige uyan

--- htdocs/news.php
+++ htdocs/news.php
@@ -29,14 +29,6 @@ $folder="contents/news/";
 
 <body style="background-color:#939598; ">
 <?php require "headermenu.php" ?>
-
-<div class="container" style="padding:50px">
-    <div class="row">
-        <div id="uyan_frame" style="width:100%"></div>
-        <script type="text/javascript" src="http://v2.uyan.cc/code/uyan.js?uid=2078754"></script>
-    </div>
-</div>
-
                     
 <div class="container" style="padding:30px;">
     <div class="row">
