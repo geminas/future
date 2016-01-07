bee26b8cf48742a5a8a759e0282e083e6bea6afe
jiangqichen <jiangqichen@xiaoma.cn>
2016-01-08 00:02:26 +0800

    xxx

--- htdocs/news.php
+++ htdocs/news.php
@@ -133,6 +133,8 @@ function loadNews() {
             
             //NewsItems
             $.each(newsRecent, function (i,e) {
+                alert(e);
+
                 if ($(".headline").length) {
                     $("#newslist").append(createNews(e));
                 } else {
