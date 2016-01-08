6276910d1d9789d1ca52bfd5414464d6667f9727
jiangqichen <jiangqichen@xiaoma.cn>
2016-01-08 14:56:35 +0800

    <?php echo http://.['HTTP_HOST']./; ?>footprints_en

--- htdocs/footprints.php
+++ htdocs/footprints.php
@@ -114,10 +114,10 @@
                                         <div class="row" style="margin:0">
                                             <div class="col-xs-6 col-sm-12" style="padding:0">
                                                 <div class="hidden-xs">
-                                                <a class="" href="FFevent/p/<?php echo $event['code']; ?>.html"><img src="contents/eventthumb/<?php echo $event['code']; ?>.jpg" alt=""></a>
+                                                <a class="" href="<?php echo "http://".$_SERVER['HTTP_HOST']."/"; ?>footprints/<?php echo $event['id']; ?>.html"><img src="contents/eventthumb/<?php echo $event['code']; ?>.jpg" alt=""></a>
                                                 </div>
                                                 <div class="hidden-sm hidden-md hidden-lg">
-                                                <a class="" href="FFevent/p/<?php echo $event['code']; ?>.html"><img src="contents/eventthumb_b/<?php echo $event['code']; ?>.jpg" alt=""></a>
+                                                <a class="" href="<?php echo "http://".$_SERVER['HTTP_HOST']."/"; ?>footprints/<?php echo $event['id']; ?>.html"><img src="contents/eventthumb_b/<?php echo $event['code']; ?>.jpg" alt=""></a>
                                                 </div>
                                             </div>
                                             <div class="col-xs-6 hidden-sm hidden-md hidden-lg cbp-caption-infobox" style="padding-left: 5%">
@@ -130,7 +130,7 @@
                                                 </div>
                                                 
                                                 <div class="gdhbutton buttonblack">
-                                                    <a href="FFevent/p/<?php echo $event['code']; ?>.html">MORE</a>
+                                                    <a href="<?php echo "http://".$_SERVER['HTTP_HOST']."/"; ?>footprints/<?php echo $event['id']; ?>.html">MORE</a>
                                                 </div>
                                             </div>
                                             
@@ -149,7 +149,7 @@
                                             </div>
                                             
                                             <div class="gdhbutton buttonwhite">
-                                                <a href="FFevent/p/<?php echo $event['code']; ?>.html">MORE</a>
+                                                <a href="<?php echo "http://".$_SERVER['HTTP_HOST']."/"; ?>footprints/<?php echo $event['id']; ?>.html">MORE</a>
                                             </div>
                                         </div>
                                     </div>
