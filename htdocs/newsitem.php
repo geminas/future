8445132f95c87b9614bafbb999ff611184347c39
jiangqichen <jiangqichen@xiaoma.cn>
2016-01-10 11:49:08 +0800

    qu

--- htdocs/newsitem.php
+++ htdocs/newsitem.php
@@ -53,8 +53,7 @@ $folder="contents/news/";
                 <span class="publishTime"><?php echo $newsitem['publishTime'];?></span>
                 <!-- <div class="row"> -->
                 <div class="bdsharebuttonbox col-sm-8"><a href="#" class="bds_more" data-cmd="more"></a><a href="#" class="bds_more" data-cmd="more">分享</a></div>
-                    <script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"1","bdSize":"24"},"share":{}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
-                </div>
+                <script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"1","bdSize":"24"},"share":{}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
                 <!-- </div> -->
             </div>
 
