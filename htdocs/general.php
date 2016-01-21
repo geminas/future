c507e2f956603cc7c5fe4c8a48d1bc4fb5da87dc
jiangqichen <jiangqichen@xiaoma.cn>
2016-01-22 00:09:03 +0800

    test

--- htdocs/general.php
+++ htdocs/general.php
@@ -103,13 +103,13 @@ function queryDB ($q) {
 }
 
 function escape_string($string) {
-    $dbc = mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    $dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
     return mysqli_real_escape_string($dbc, $string);
 }
 
 function accessDB($q) {
 	// Make the connection:
-	$dbc = mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	$dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 	$result=array();
 	
 	// If no connection could be made, trigger an error:
