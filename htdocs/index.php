97708cdae94b9de82301fd0a6bf77520afa75984
jiangqichen <jiangqichen@xiaoma.cn>
2016-01-21 19:26:37 +0800

    bubiubiu

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,6 +2,7 @@
 <html>
 
 <head>
+    <?php print_r("indexindexindexindexindexindexindexindex") ?>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta id="viewport" name="viewport" content="width=device-width">
