8187be17005e6d47cdc9114ac501668c2fd12918
jiangqichen <jiangqichen@xiaoma.cn>
2016-02-18 15:23:40 +0800

    update prize

--- htdocs/headermenu.php
+++ htdocs/headermenu.php
@@ -10,7 +10,7 @@
         <span style="width:0.5%" class="HPtopmenu"></span>
         <a href="<?php echo "http://".$_SERVER['HTTP_HOST']."/index.php"; ?>"><span style="width:12.7%" class="HPtopmenu">首页</span></a>
         <a href="<?php echo "http://".$_SERVER['HTTP_HOST']."/people.php"; ?>"><span style="width:13.8%" class="HPtopmenu1">未来·人物</span></a>
-        <a href="<?php echo "http://".$_SERVER['HTTP_HOST']."/prize.php"; ?>"><span style="width:13.8%" class="HPtopmenu2">未来·大奖</span></a>
+        <a href="<?php echo "http://".$_SERVER['HTTP_HOST']."/prize.php"; ?>"><span style="width:13.8%" class="HPtopmenu2">未来·科学大奖</span></a>
         <a href="<?php echo "http://".$_SERVER['HTTP_HOST']."/footprints.php"; ?>"><span style="width:13.8%" class="HPtopmenu3">未来·足迹</span></a>
         <a href="<?php echo "http://".$_SERVER['HTTP_HOST']."/news.php"; ?>"><span style="width:13.8%" class="HPtopmenu4">未来·资讯</span></a>
         <a class="header-language-cn" href="<?php echo "http://".$_SERVER['HTTP_HOST']."/index_en.php"; ?>"><img src="/contents/language/l_chinese.png" style="width: 100%" alt=""> </a> 
@@ -24,7 +24,7 @@
         <div id="barmenu-content" style="display: none; position: absolute; right:0; width:66%; background-color: rgb(80,80,80); z-index:99999; padding: 10% 0">
             <a href="<?php echo "http://".$_SERVER['HTTP_HOST']."/index.php"; ?>"><div class="HPtopmenucollapse" style="padding: 10%;">首页</div></a>
             <a href="<?php echo "http://".$_SERVER['HTTP_HOST']."/people.php"; ?>"><div class="HPtopmenucollapse" style="padding: 10%;">未来·人物</div></a>
-            <a href="<?php echo "http://".$_SERVER['HTTP_HOST']."/prize.php"; ?>"><div class="HPtopmenucollapse" style="padding: 10%;">未来·大奖</div></a>
+            <a href="<?php echo "http://".$_SERVER['HTTP_HOST']."/prize.php"; ?>"><div class="HPtopmenucollapse" style="padding: 10%;">未来·科学大奖</div></a>
             <a href="<?php echo "http://".$_SERVER['HTTP_HOST']."/footprints.php"; ?>"><div class="HPtopmenucollapse" style="padding: 10%;">未来·足迹</div></a>
             <a href="<?php echo "http://".$_SERVER['HTTP_HOST']."/news.php"; ?>"><div class="HPtopmenucollapse" style="padding: 10%;">未来·资讯</div></a>
             <a href="<?php echo "http://".$_SERVER['HTTP_HOST']."/index_en.php"; ?>"><div class="HPtopmenucollapse" style="padding: 10%;">英文</div></a>
