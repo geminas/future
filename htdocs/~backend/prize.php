1a4c34d01a70c73597e5244c4ff11ae027d42a92
jiangqichen <jqcMac@jiangqichendeMacBook-Pro.local>
2016-03-10 14:23:07 +0800

    prize

--- htdocs/~backend/prize.php
+++ htdocs/~backend/prize.php
@@ -9,7 +9,7 @@ include ('includes/header.php');
 <script type="text/javascript" charset="utf-8" src="ueditor/ueditor.config.js"></script>
 <script type="text/javascript" charset="utf-8" src="ueditor/ueditor.all.min.js"> </script>
 <script type="text/javascript" charset="utf-8" src="ueditor/lang/zh-cn/zh-cn.js"></script>
-<div id="prize_container" style="display:none"></div>
+<div id="prize_form_container" style="display:none"></div>
 <script>
 
 $(document).ready( function () {
