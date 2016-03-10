1320a6c3e25568b7effed82766615f6fce294153
jiangqichen <jqcMac@jiangqichendeMacBook-Pro.local>
2016-03-10 15:45:57 +0800

    publishtime -> code

--- htdocs/news.php
+++ htdocs/news.php
@@ -12,7 +12,7 @@ include "metalinks.php";
 $newsRecommended=queryDB_array("
     select id,code,title from news
     where recommendation > 0 and status <> 0
-    order by publishTime desc
+    order by code desc
     limit 10;
 ");
 $folder="contents/news/";
