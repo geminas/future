e71ef43c21c086b8584e2e59c774f5139670fa8b
jiangqc5656 <jiangqc5656@gmail.com>
2016-03-14 11:16:03 +0800

    publish tie

--- htdocs/~backend/apis/general.php
+++ htdocs/~backend/apis/general.php
@@ -11,6 +11,25 @@ function print_r2($val){
     echo  '</pre>';
 }
 
+Date.prototype.Format = function(fmt)   
+{ //author: meizz   
+  var o = {   
+    "M+" : this.getMonth()+1,                 //月份   
+    "d+" : this.getDate(),                    //日   
+    "h+" : this.getHours(),                   //小时   
+    "m+" : this.getMinutes(),                 //分   
+    "s+" : this.getSeconds(),                 //秒   
+    "q+" : Math.floor((this.getMonth()+3)/3), //季度   
+    "S"  : this.getMilliseconds()             //毫秒   
+  };   
+  if(/(y+)/.test(fmt))   
+    fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));   
+  for(var k in o)   
+    if(new RegExp("("+ k +")").test(fmt))   
+  fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));   
+  return fmt;   
+}  
+
 //Input processing and sanitization
 
 function processRequestArguments() {
@@ -102,7 +121,7 @@ function UpdateFFV($argin) {
                     $tempStatus=queryDB_row("select * from news where id = '{$argin['id']}'");
                     $oldStatus=$tempStatus['status'];
                     if ($oldStatus==0) {
-                        $time1 = date("yyyy-MM-dd HH:mm:ss", time());
+                        $time1 = date("Y-m-d H:i:s", time());
                         $SetClause.="`publishTime`='$time1'";
                         continue;
                     }
