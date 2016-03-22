5d69478db4158d541e91540136f55329d4ac7346
jiangqc5656 <jiangqc5656@gmail.com>
2016-03-22 21:13:13 +0800

    rss

--- htdocs/rss.php
+++ htdocs/rss.php
@@ -12,20 +12,18 @@
 	  } 
 	}
 	
-	$items=queryDB_array("select * from news");
+	$items=queryDB_array("select * from news where recommendation > 0 and status <> 0 order by recommendation asc, publishTime desc");
 	// $xml = new SimpleXMLElement('<rss version="2.0"/>');
 	$rss=new SimpleXMLExtended('<rss version="2.0"/>');
 	$channel=$rss->addChild('channel');
 	$channel->addChild('title', "未来论坛");
-	$channel->addChild('description', "未来论坛XML");
-	$channel->addChild('link', "http://www.baidu.com");
-	$channel->addChild('generator', "http://www.google.com");
+	$channel->addChild('description', "未来论坛RSS");
+	$channel->addChild('link', "http://www.futureforum.org.cn/");
 
 	foreach($items as $item) {
 	    $itemXml = $channel->addChild('item');
 	    $itemXml->addChild('title', "{$item['title']}");
-	    $link=$itemXml->addChild('link');
-	    $link->addCData('https://bbs.sjtu.edu.cn/bbstcon,board,PartTime,reid,1453027326.html');
+	    $itemXml->addChild('link', "http://www.futureforum.org.cn/news/{$item['id']}.html");
 	    $itemXml->addChild('author', "{$item['author']}");
 	    $itemXml->addChild('pubDate', "{$item['publishTime']}");
 	}
